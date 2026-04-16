1754f04bb777d899c0f6ae4897101bce22cbaf39
danielepucci03 <danielepucci03@gmail.com>
2026-04-16 15:44:45 +0200

    first commit

---
+++ react-dc-comics/src/App.jsx
@@ -0,0 +1,23 @@
+
+import AppHeader from './components/AppHeader'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import AppMain from './components/AppMain'
+
+
+function App() {
+
+
+  return (
+    <>
+
+      <AppHeader />
+      <AppMain />
+      <AppFooter />
+
+
+    </>
+  )
+}
+
+export default App
