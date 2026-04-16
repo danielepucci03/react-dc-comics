19e75bde19378c22a49a70cc668014947eb86d40
danielepucci03 <danielepucci03@gmail.com>
2026-04-16 18:48:26 +0200

    Remove unused imports in App.jsx

--- react-dc-comics/src/App.jsx
+++ react-dc-comics/src/App.jsx
@@ -1,9 +1,7 @@
 
 import AppHeader from './components/AppHeader'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import AppMain from './components/AppMain'
-
+import AppFooter from './components/AppFooter'
 
 function App() {
 
