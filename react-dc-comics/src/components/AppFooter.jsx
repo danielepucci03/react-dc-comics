2ea7bcd7a5c36cdf75240e8310a5f03fcc7a7972
danielepucci03 <danielepucci03@gmail.com>
2026-04-17 10:49:26 +0200

    Add AppFooter component and restructure AppHeader for better semantics

--- react-dc-comics/src/components/AppFooter.jsx
+++ react-dc-comics/src/components/AppFooter.jsx
@@ -1,6 +1,32 @@
+import FooterFb from '../assets/dc-comics-1/img/footer-facebook.png'
+import FooterPer from '../assets/dc-comics-1/img/footer-periscope.png'
+import FooterPin from '../assets/dc-comics-1/img/footer-pinterest.png'
+import FooterTw from '../assets/dc-comics-1/img/footer-twitter.png'
+import FooterYt from '../assets/dc-comics-1/img/footer-youtube.png'
+
 export default function AppFooter() {
 
     return (
-        <footer>Footer</footer>
+        <footer>
+            <button>SIGN-UP NOW!</button>
+
+            <a href="">FOLLOW US</a>
+
+            <a href="">
+                <img src={FooterFb} alt="" />
+            </a>
+            <a href="">
+                <img src={FooterTw} alt="" />
+            </a>
+            <a href="">
+                <img src={FooterYt} alt="" />
+            </a>
+            <a href="">
+                <img src={FooterPin} alt="" />
+            </a>
+            <a href="">
+                <img src={FooterPer} alt="" />
+            </a>
+        </footer>
     )
 }
--- react-dc-comics/src/components/AppHeader.jsx
+++ react-dc-comics/src/components/AppHeader.jsx
@@ -1,16 +1,22 @@
 export default function AppHeader() {
     return (
         <header>
-            <a href="">CHARACTERS</a>
-            <a href="">GAMES</a>
-            <a href="">MOVIES</a>
-            <a href="">TV</a>
-            <a href="">GAMES</a>
-            <a href="">COLLECTIBLES</a>
-            <a href="">VIDEOS</a>
-            <a href="">FANS</a>
-            <a href="">NEWS</a>
-            <a href="">SHOP</a>
+
+            <nav>
+
+                <a href="">CHARACTERS</a>
+                <a href="">GAMES</a>
+                <a href="">MOVIES</a>
+                <a href="">TV</a>
+                <a href="">GAMES</a>
+                <a href="">COLLECTIBLES</a>
+                <a href="">VIDEOS</a>
+                <a href="">FANS</a>
+                <a href="">NEWS</a>
+                <a href="">SHOP</a>
+
+            </nav>
+
         </header>
     )
 
