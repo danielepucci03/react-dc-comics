afaa31df1b48f815971faeaa25ef2c93987f7170
danielepucci03 <danielepucci03@gmail.com>
2026-04-17 11:00:19 +0200

    Refactor AppMain component to enhance structure and add image sections

--- react-dc-comics/src/components/AppMain.jsx
+++ react-dc-comics/src/components/AppMain.jsx
@@ -1,6 +1,28 @@
+import MainDC from '../assets/dc-comics-1/img/buy-comics-digital-comics.png'
+import MainM from '../assets/dc-comics-1/img/buy-comics-merchandise.png'
+import MainSL from '../assets/dc-comics-1/img/buy-comics-shop-locator.png'
+import MainS from '../assets/dc-comics-1/img/buy-comics-subscriptions.png'
+import MainPV from '../assets/dc-comics-1/img/buy-dc-power-visa.svg'
+
+
+
 export default function AppMain() {
+
     return (
-        <main>Main</main>
+        <main>
+
+            <section>
+
+            </section>
+
+            <section className="image">
+                <img src={MainDC} alt="" />
+                <img src={MainM} alt="" />
+                <img src={MainSL} alt="" />
+                <img src={MainS} alt="" />
+                <img src={MainPV} alt="" />
+            </section>
+        </main>
     )
 
 }
