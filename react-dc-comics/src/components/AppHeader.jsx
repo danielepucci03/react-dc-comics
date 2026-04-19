f8d18862b2b34ef253a29be9d211f0aeedc39e08
danielepucci03 <danielepucci03@gmail.com>
2026-04-19 17:15:30 +0200

    Refactor AppHeader to use CHARACTERS array for navigation links, improving maintainability

--- react-dc-comics/src/components/AppHeader.jsx
+++ react-dc-comics/src/components/AppHeader.jsx
@@ -2,6 +2,70 @@ import AppLogo from '../assets/dc-comics-1/img/dc-logo.png'
 
 
 export default function AppHeader() {
+
+    const CHARACTERS = [
+        {
+            id: 1,
+            text: CHARACTERS,
+            path: '/characters'
+
+        },
+        {
+            id: 2,
+            text: COMICS,
+            path: '/comics'
+
+        },
+        {
+            id: 3,
+            text: MOVIES,
+            path: '/movies'
+
+        },
+        {
+            id: 4,
+            text: TV,
+            path: '/tv'
+
+        },
+        {
+            id: 5,
+            text: GAMES,
+            path: '/games'
+
+        },
+        {
+            id: 6,
+            text: COLLECTIBLES,
+            path: '/collectibles'
+
+        },
+        {
+            id: 7,
+            text: VIDEOS,
+            path: '/videos'
+
+        },
+        {
+            id: 8,
+            text: FANS,
+            path: '/fans'
+
+        },
+        {
+            id: 9,
+            text: NEWS,
+            path: '/news'
+
+        },
+        {
+            id: 10,
+            text: SHOP,
+            path: '/shop'
+
+        }
+    ]
+
     return (
         <header>
 
@@ -11,16 +75,10 @@ export default function AppHeader() {
 
                 <nav>
 
-                    <a href="">CHARACTERS</a>
-                    <a href="" className='comics'>COMICS</a>
-                    <a href="">MOVIES</a>
-                    <a href="">TV</a>
-                    <a href="">GAMES</a>
-                    <a href="">COLLECTIBLES</a>
-                    <a href="">VIDEOS</a>
-                    <a href="">FANS</a>
-                    <a href="">NEWS</a>
-                    <a href="">SHOP</a>
+                    {CHARACTERS.map((item) => (
+                        <a href={item.path} key={item.id}>{item.text}</a>
+                    ))
+                    }
 
                 </nav>
             </section>
